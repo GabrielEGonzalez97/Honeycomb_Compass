51d82eea83a2b7423e489f5506ad8ab9b52699c9
GabrielEGonzalez97 <gabrielenrique.gonzalez97@gmail.com>
2021-10-23 17:35:53 -0300

    user can enter size of bee panel and it is rendered

---
+++ code/src/app/components/honeycomb/honeycomb.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-honeycomb',
+  templateUrl: './honeycomb.component.html',
+  styleUrls: ['./honeycomb.component.css']
+})
+export class HoneycombComponent {
+  colArray : number[] = [];
+  rowArray: number[] = [];
+  columns = 5;
+  rows = 5;
+  min = 0;
+  max = 20;
+
+  constructor() { }
+
+  setShapeHoneycomb()
+  {
+    this.checkHoneycombDimensions();
+    this.colArray = [];
+    this.rowArray = [];
+    for(let i = 0; i < this.columns; i++)
+    {
+      this.colArray.push(i);
+    }
+    for(let i = 0; i < this.rows; i++)
+    {
+      this.rowArray.push(i);
+    }
+  }
+
+  private checkHoneycombDimensions()
+  {
+    if(this.rows < this.min)
+    {
+      this.rows = this.min;
+    }
+    else if(this.rows > this.max)
+    {
+      this.rows = this.max;
+    }
+
+    if(this.columns < this.min)
+    {
+      this.columns = this.min;
+    }
+    else if(this.columns > this.max)
+    {
+      this.columns = this.max;
+    }
+  }
+}
